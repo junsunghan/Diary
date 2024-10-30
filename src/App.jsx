69443d933569fc0eac6ecc18d46914c12ce45df8
junsunghan <hjs8213073@gmail.com>
2024-10-30 17:05:55 +0900

    2. 일기 관리 기능 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
 import Button from "./components/Button";
+import Edit from "./pages/Edit";
 import Header from "./components/Header";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+
+import { Routes, Route } from "react-router-dom";
+import { useReducer, useRef, createContext } from "react";
 
 import { getEmotionImage } from "./util/get-emotion-image";
 
@@ -13,47 +16,117 @@ import { getEmotionImage } from "./util/get-emotion-image";
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
-function App() {
-  const nav = useNavigate();
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
 
-  const onClickButton = () => {
-    nav("/new");
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // 새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
   };
 
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-      <Button
-        text={"123"}
-        type={"DEFAULT"}
+      <button
         onClick={() => {
-          console.log("123번 버튼 클릭!");
+          onCreate(new Date().getTime(), 1, "하이");
         }}
-      />
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
+      >
+        일기 추가
+      </button>
+      <button
         onClick={() => {
-          console.log("123번 버튼 클릭!");
+          onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다");
         }}
-      />
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
+      >
+        일기 수정
+      </button>
+      <button
         onClick={() => {
-          console.log("123번 버튼 클릭!");
+          onDelete(1);
         }}
-      />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      >
+        일기 삭제
+      </button>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={{
+            onCreate,
+            onUpdate,
+            onDelete,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="*" element={<Notfound />} />
+            <Route path="/edit/:id" element={<Edit />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
