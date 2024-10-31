8b0c700f9223c71c060ce684325cf355e4243f0f
junsunghan <hjs8213073@gmail.com>
2024-10-31 10:10:41 +0900

    3-2. Home 페이지  UI 구현 완료

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,16 +1,24 @@
 import { getEmotionImage } from "../util/get-emotion-image";
-
+import Button from "./Button";
+import "./DiaryItem.css";
 
 const DiaryItem = () => {
-    return (
-        <div className="DiaryItem">
-            <div className="img_section">
-                <img src={getEmotionImage(1)}></img>
-            </div>
-            <div className="info_section"></div>
-            <div className="button_section"></div>
-        </div>
-    );
+  const emotionId = 4;
+
+  return (
+    <div className="DiaryItem">
+      <div className={`img_section img_section_${emotionId}`}>
+        <img src={getEmotionImage(emotionId)}></img>
+      </div>
+      <div className="info_section">
+        <div className="created_date">{new Date().toLocaleDateString()}</div>
+        <div className="content">일기 컨텐츠</div>
+      </div>
+      <div className="button_section">
+        <Button text={"수정하기"} />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryItem;
