76c3677147367ca1ee51000b19b48e16c968a3a8
JUNSUNG <hjs8213073@gmail.com>
2024-11-04 23:54:29 +0900

    5-1. edit 페이지 구현 중 삭제까지

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,7 +1,34 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const Edit = () => {
   const params = useParams();
-  return <div>edit</div>;
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      onDelete(Number(params.id)); //number로 해줘야 인식이됨머징
+      console.log(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 };
 export default Edit;
