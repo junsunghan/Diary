174d90440b205692e23f27912a436f21f53977a2
junsunghan <hjs8213073@gmail.com>
2024-11-05 14:49:04 +0900

    5-2. edit 페이지 구현 완료

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -2,18 +2,44 @@ import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
-import { useContext } from "react";
-import { DiaryDispatchContext } from "../App";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
-  const { onDelete } = useContext(DiaryDispatchContext);
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext);
+  const [curDiaryItem, setCurDiaryItem] = useState();
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    );
+    if (!currentDiaryItem) {
+      window.alert("존재하지 않는 일기입니다.");
+      nav("/", { replace: true });
+    }
+
+    setCurDiaryItem(currentDiaryItem);
+  }, [params.id, data]);
 
   const onClickDelete = () => {
     if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
       onDelete(Number(params.id)); //number로 해줘야 인식이됨머징
       console.log(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  const onSubmit = (input) => {
+    if (window.confirm("일기를 정말 수정할까요?")) {
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
       nav("/", { replace: true });
     }
   };
@@ -27,7 +53,7 @@ const Edit = () => {
           <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
         }
       />
-      <Editor />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
     </div>
   );
 };
